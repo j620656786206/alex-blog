17b4e216ac428a550d42e08144c180f5c8dcbe27
Alex Yu <j620656786206@gmail.com>
2022-12-10 19:56:31 +0800

    test commit

--- src/main.js
+++ src/main.js
@@ -18,7 +18,7 @@
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-
+console.log("test");
 const exposed = {};
 if (location.search) {
   var a = document.createElement("a");
